45cd2929af37bc7c40c6603d9f2cf29effc41b80
Md. Noman Ebeny Shahid <greatsaykot@gmail.com>
2024-06-17 00:11:40 +0600

    text changed to small

--- src/components/interface/Interface.tsx
+++ src/components/interface/Interface.tsx
@@ -6,35 +6,35 @@ export default function Interface() {
       <Tabs defaultValue="veranda" className="w-full">
         <TabsList className="border-none rounded-none outline-none w-full h-16 bg-bgGrayColor">
           <TabsTrigger
-            className=" rounded-none outline-none border-r-2 px-2 h-12"
+            className=" rounded-none outline-none border-r-2 px-2 h-12 text-xs"
             value="veranda"
           >
             <p>VERANDA</p>
           </TabsTrigger>
 
           <TabsTrigger
-            className=" rounded-none outline-none px-2 h-12 border-r-2"
+            className=" rounded-none outline-none px-2 h-12 border-r-2 text-xs"
             value="gardenRoom"
           >
             GARDEN <br /> ROOM
           </TabsTrigger>
 
           <TabsTrigger
-            className=" rounded-none outline-none px-2 h-12 border-r-2"
+            className=" rounded-none outline-none px-2 h-12 border-r-2 text-xs"
             value="slidingWall"
           >
             SLIDING <br /> WALL
           </TabsTrigger>
 
           <TabsTrigger
-            className=" rounded-none outline-none px-2 h-12 border-r-2"
+            className=" rounded-none outline-none px-2 h-12 border-r-2 text-xs"
             value="carPort"
           >
             CARPORT
           </TabsTrigger>
 
           <TabsTrigger
-            className=" border-none rounded-none outline-none px-2 h-16"
+            className=" border-none rounded-none outline-none px-2 h-16 text-xs"
             value="sunProtection"
           >
             SUN <br /> PROTECTION
