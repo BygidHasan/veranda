25d4d5a36674490c5d4dfcda334d4645d7fd5451
BygidHasan <bygidhasan6@gmail.com>
2024-06-16 16:33:35 +0600

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Canvas3D from "./models/Canvas3D";
+import Interface from "./components/Interface";
+
+function App() {
+  return (
+    <>
+      <Canvas3D />
+      <Interface />
+    </>
+  );
+}
+
+export default App;
---
+++ src/models/Canvas3D.tsx
@@ -0,0 +1,5 @@
+export default function Canvas3D() {
+  return (
+    <div className="fixed top-0 bg-teal-200 h-[50vh] w-full">3DCanvas</div>
+  );
+}
