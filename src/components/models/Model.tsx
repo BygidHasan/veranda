ee54d0f5cf990d51b4dfd58efcfcb4d572d4744e
Md. Noman Ebeny Shahid <greatsaykot@gmail.com>
2024-06-30 19:18:47 +0600

    model update

--- src/components/models/Model.tsx
+++ src/components/models/Model.tsx
@@ -17,6 +17,8 @@ export function Model(props: MeshProps) {
     './models/vereanda_express.glb'
   ) as unknown as GLTFResult;
 
+  console.log(materials);
+
   return (
     <group {...(props as GroupProps)} dispose={null as unknown as () => void}>
       <group position={[0.326, 1.159, 0.494]} rotation={[0, 1.571, 0]}>
@@ -41,50 +43,65 @@ export function Model(props: MeshProps) {
         />
       </group>
       <mesh
-        geometry={nodes.Column_alu.geometry}
+        geometry={nodes.Column_Back_Left.geometry}
         material={materials.Black_matel}
-        position={[0.032, 0, 2.991]}
+        position={[-0.604, 0.6, 0.991]}
       />
       <mesh
         geometry={nodes.Top_alu_1.geometry}
         material={materials.Black_matel}
-        position={[-0.036, 2.568, 2.559]}
+        position={[-0.036, 1.182, 2.559]}
       />
       <mesh
         geometry={nodes.Top_alu_2_a.geometry}
         material={materials.Black_matel}
-        position={[0.032, 0, 2.991]}
+        position={[-0.036, 1.095, 4.138]}
       />
       <mesh
         geometry={nodes.Top_alu_3_a.geometry}
         material={materials.Black_matel}
-        position={[0.032, 0, 2.991]}
+        position={[-0.036, 1.095, 4.154]}
       />
       <mesh
         geometry={nodes.Top_alu_2_b.geometry}
         material={materials.Black_matel}
-        position={[0.032, 0, 2.991]}
+        position={[-0.036, 1.097, 4.135]}
       />
       <mesh
         geometry={nodes.Top_alu_3_b.geometry}
         material={materials.Black_matel}
-        position={[0.032, 0, 2.991]}
+        position={[-0.036, 1.098, 4.139]}
       />
       <mesh
         geometry={nodes.Back_alu.geometry}
         material={materials.Black_matel}
-        position={[0.032, 0, 2.991]}
+        position={[-0.036, 1.269, 0.942]}
       />
       <mesh
         geometry={nodes.Roof_top_alu.geometry}
         material={materials.Black_matel}
-        position={[-0.036, 2.568, 2.559]}
+        position={[-0.035, 1.182, 2.559]}
       />
       <mesh
         geometry={nodes.Glass_opal.geometry}
         material={materials.glass_poly_clear}
-        position={[-0.036, 2.618, 2.559]}
+        position={[-0.036, 1.235, 2.491]}
         scale={[0.945, 1, 1]}
+      />
+      <mesh
+        geometry={nodes.Column_Front_right.geometry}
+        material={materials.Black_matel}
+        position={[0.532, 0.5, 4.127]}
+      />
+      <mesh
+        geometry={nodes.Column_Front_Left.geometry}
+        material={materials.Black_matel}
+        position={[-0.604, 0.5, 4.127]}
+      />
+      <mesh
+        geometry={nodes.Column_Back_Right.geometry}
+        material={materials.Black_matel}
+        position={[0.532, 0.6, 0.991]}
       />
     </group>
   );
